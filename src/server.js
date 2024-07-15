7dc1da460fdb8cf8c835cc1332110c4c4f249fac
Yuliia Diachenko <zirkanew82@gmail.com>
2024-07-15 10:09:31 +0300

    update

--- src/server.js
+++ src/server.js
@@ -23,6 +23,7 @@ export default async function setupServer() {
     const contacts = await getAllContacts();
 
     res.status(200).json({
+      status: 200,
       message: "Successfully found contacts!",
       data: contacts,
     });
